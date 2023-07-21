858ce18843267782e29447a63808e1ae53643bc1
Sebastian Vasquez <sebitas0489@gmail.com>
2023-07-21 15:17:18 -0500

    Renderizado de canvas con los elementos recorriendo cada array

--- game.js
+++ game.js
@@ -32,7 +32,22 @@ function startGame() {
   game.font = elementsSize + "px Verdana";
   game.textAlign = "end";
 
-  for (let i = 1; i <= 10; i++) {
-    game.fillText(emojis["X"], elementsSize, elementsSize * i);
-  }
+  ///*** Creamos las variables para acceder a los elemntos desde las posiciones ***///
+  const map = maps[0];
+  //Creamos la variable de las filas del mapa y Limpiamos el string con el metodo ".trim()" y creamos un nuevo arreglo a partir del string con ".split()"
+  const mapRows = map.trim().split("\n");
+  //Creamos un nuevo array con .map
+  const mapRowsCols = mapRows.map((row) => row.trim().split(""));
+  console.log({ map, mapRows, mapRowsCols });
+
+  //Recorremos las filas y columnas para asi asignar cada posición y Renderizar de una manera mas legible en el codigo.
+  mapRowsCols.forEach((row, rowIndex) => {
+    row.forEach((colum, columIndex) => {
+      const emoji = emojis[colum];
+      const posX = elementsSize * (columIndex + 1);
+      const posY = elementsSize * (rowIndex + 1);
+      game.fillText(emoji, posX, posY);
+      console.log({ row, rowIndex, colum, columIndex });
+    });
+  });
 }
